763915cd1b4a61cd1ac504a8bfde084ca7e84dcb
Leandro Alfonso <leamanya24.6@gmail.com>
2021-09-22 20:47:22 -0300

    Added NotNull annotation to getDefault method in MessageEnum

--- src/main/java/com/github/alfonsoleandro/mputils/misc/MessageEnum.java
+++ src/main/java/com/github/alfonsoleandro/mputils/misc/MessageEnum.java
@@ -1,5 +1,7 @@
 package com.github.alfonsoleandro.mputils.misc;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Simple interface containing methods that an enum containing a list of messages for
  * the {@link com.github.alfonsoleandro.mputils.managers.MessageSender} should have.
@@ -10,6 +12,6 @@ public interface MessageEnum{
      * Gets the default value for a message in case the value from the file returns null.
      * @return A default value for this message in case the configurable value returns null.
      */
-    String getDefault();
+    @NotNull String getDefault();
 
 }
